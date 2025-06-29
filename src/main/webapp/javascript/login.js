2e7697b634b4dec4129b61ab54764eead5cb9de2
sevalk02 <144692258+sevalk02@users.noreply.github.com>
2025-06-29 22:25:05 +0200

    azure testen inloggen

--- src/main/webapp/javascript/login.js
+++ src/main/webapp/javascript/login.js
@@ -3,12 +3,14 @@ document.getElementById("loginForm").addEventListener("submit", async function (
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const baseUrl = window.location.pathname.split("/")[1] || "";
 
-    const response = await fetch("/NeuroplannerHU/restservices/auth/login", {
+    const response = await fetch(`/${baseUrl}/restservices/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ gebruikersnaam: username, wachtwoord: password })
     });
+
 
     const result = await response.json();
 
